4802a16203615eb4d3e3098fd39a1bb28a46075e
Evgenia Isaeva <evgenia.isaeva297@gmail.com>
2023-02-09 21:40:15 +0300

    Interactive rating component

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const card = document.querySelector(".card");
+const form = document.querySelector(".card__form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (!form.querySelector('input[name="rating"]:checked')) return;
+
+  const rating = form.querySelector('input[name="rating"]:checked').value;
+  const ratingOut = document.querySelector(".rating__out");
+  ratingOut.textContent = rating;
+
+  card.classList.add("submitted");
+});
